58bad3c4e03746e83701cee96e1b21db8975874f
ldeavila <luis@ideanerd.com>
2014-01-07 17:16:49 -0800

    code cleanup.

--- server/server.js
+++ server/server.js
@@ -24,9 +24,56 @@ if ('development' == server.get('env')) {
     server.use(express.errorHandler());
 }
 
+
 server.get('/', function (req, res) {
     res.sendfile(path.join(__dirname, '../public/static/index.html'));
 });
+
+server.get('/about', function (req, res) {
+    res.sendfile(path.join(__dirname, '../public/static/about.html'));
+});
+
+server.get('/cancer-diets', function (req, res) {
+    res.sendfile(path.join(__dirname, '../public/static/cancer-diets.html'));
+});
+
+server.get('/clinics', function (req, res) {
+    res.sendfile(path.join(__dirname, '../public/static/clinics.html'));
+});
+
+server.get('/contact-us', function (req, res) {
+    res.sendfile(path.join(__dirname, '../public/static/contact-us.html'));
+});
+
+
+server.get('/disclaimer', function (req, res) {
+    res.sendfile(path.join(__dirname, '../public/static/disclaimer.html'));
+});
+
+server.get('/practitioners', function (req, res) {
+    res.sendfile(path.join(__dirname, '../public/static/practitioners.html'));
+});
+
+server.get('/privacy-policy', function (req, res) {
+    res.sendfile(path.join(__dirname, '../public/static/privacy-policy.html'));
+});
+
+server.get('/resources', function (req, res) {
+    res.sendfile(path.join(__dirname, '../public/static/resources.html'));
+});
+
+server.get('/site-map', function (req, res) {
+    res.sendfile(path.join(__dirname, '../public/static/site-map.html'));
+});
+
+server.get('/terms-of-use', function (req, res) {
+    res.sendfile(path.join(__dirname, '../public/static/terms-of-use.html'));
+});
+
+server.get('/treatments', function (req, res) {
+    res.sendfile(path.join(__dirname, '../public/static/treatments.html'));
+});
+
 
 http.createServer(server).listen(server.get('port'), function () {
     console.log('Express server listening on port ' + server.get('port'));
