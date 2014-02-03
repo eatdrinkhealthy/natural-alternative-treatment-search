3312823284f2b0e7d1d29394ece34ad213cd31a7
Spencer Sims <git@spencersims.com>
2014-02-03 12:01:20 -0800

    when user clicks on locate, the clinics that are nearest to the browsers gps location are returned

--- public/app/clinics/js/clinics-module.js
+++ public/app/clinics/js/clinics-module.js
@@ -8,7 +8,25 @@ clinicsModule.controller('clinicsController', ['$scope', '$location', 'clinicsSe
 	clinicsService.getClinics().then(function (clinics) {
 		$scope.clinics = clinics;
 	});
-
+	
+	$scope.data = {};
+	var getLocation = function() {
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(showPosition);
+		} else {
+			alert("Geolocation is not supported by this browser.");
+		}
+	};
+	var showPosition = function(position) {
+		$scope.clinics = undefined;
+		//$scope.gps = "Latitude: " + position.coords.latitude + " Longitude: "+ position.coords.longitude;
+		clinicsService.getClinicsByProximity(position.coords.latitude,position.coords.longitude,$scope.data.miles).then(function (clinics) {
+			$scope.clinics = clinics;
+		});
+	};
+	$scope.locate = function(){
+		getLocation();
+	};
 }]);
 
 clinicsModule.controller('clinicController', ['$scope', '$location', 'clinicsService', function ($scope, $location, clinicsService) {
@@ -46,7 +64,18 @@ clinicsModule.factory('clinicsService', ['$http', function ($http) {
 				;
 			});
 			return promise;
-		}
+		},
+		getClinicsByProximity: function (lat,lng,miles) {
+			var promise = $http.get('http://localhost/api/clinics/local/'+lng+'/'+lat+'/'+miles).then(function (response) {
+				if (response.status === 200) {
+					return response.data;
+				} else {
+					window.alert(response.data.message);
+					return response.data
+				}
+			});
+			return promise;
+		},
 	}
 	return ClinicsService;
 }]);
